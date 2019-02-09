714322760acd395f7a48a7475e1c9f44c77bbbf0
IvanStack <iivanangelovski9@gmail.com>
2019-02-09 23:03:38 +0100

    add basic elements

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// CSS
+import './App.css'
 
 class App extends Component {
-  render() {
+  state = {
+    pseudo: this.props.match.params.pseudo
+  }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='box'>
+        <h1>Bonjour {this.state.pseudo}</h1>
+        <div className='cards'>
+          <div className='card'>
+            <h2>Une Carte</h2>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
