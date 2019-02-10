865a77a65b50a234610572bb4c9861e0157096db
IvanStack <iivanangelovski9@gmail.com>
2019-02-10 13:18:58 +0100

    add show card

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 import './App.css'
 import Header from './components/Header'
+import recettes from './recettes'
+import Admin from './components/Admin'
+import Card from './components/Card';
 
 class App extends Component {
   state = {
-    pseudo: this.props.match.params.pseudo
+    pseudo: this.props.match.params.pseudo,
+    recettes: {}
   }
 
+  chargerExemple = () => this.setState({ recettes })
+
   render () {
+
+    const cards = Object.keys(this.state.recettes)
+    .map(key => <Card key={key} details={this.state.recettes[key]}/>)
+
     return (
       <div className='box'>
         <Header pseudo={this.state.pseudo}></Header>
         <div className='cards'>
-          <div className='card'>
-            <h2>Une Carte</h2>
-          </div>
+          {cards}
         </div>
+        <Admin chargerExemple={this.chargerExemple}/>
       </div>
     )
   }
