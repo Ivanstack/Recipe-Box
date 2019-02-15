e0d8a4eed3821bb8f247a7cd803a5ed6bef472a5
IvanStack <iivanangelovski9@gmail.com>
2019-02-15 10:36:25 +0100

    add url exemple recette and default

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Card = ({ details }) => {
+const Card = ({ details, default_img }) => {
 
     const ingredients = details.ingredients
         .split(',')
@@ -13,7 +13,7 @@ const Card = ({ details }) => {
     return (
         <div className="card">
             <div className="image">
-                <img src={(details.image)} alt={details.nom}/>
+                <img src={(details.image)} alt={details.nom} onError={(e) => {e.target.src = 'https://firebasestorage.googleapis.com/v0/b/recipe-box-cb02c.appspot.com/o/default.jpeg?alt=media&token=3b0cbc24-a561-4b4f-aaa0-7e49dd2c2a8b'}} />
             </div>
             <div className="recette">
                 <h2>{details.nom}</h2>
