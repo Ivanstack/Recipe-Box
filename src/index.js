1327e684c70dd2eb58318940587f788f89ce562e
Ivanstack <36751399+Ivanstack@users.noreply.github.com>
2019-09-28 16:28:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-// Routes
 import MainRouter from './Routes'
 
 ReactDOM.render(<MainRouter/>, document.getElementById('root'));
